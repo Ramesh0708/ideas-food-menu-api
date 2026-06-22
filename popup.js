a3b0d2ab4eb03a6507efd7b311d3db2a71d2960f
Ramesh Choudhary <61725029+Ramesh0708@users.noreply.github.com>
2026-06-23 04:29:24 +0530

    Fetch menu data from API and update UI

--- popup.js
+++ popup.js
@@ -1,26 +1,29 @@
-const data = {
-    date: "2026-06-22",
-    breakfast: "Vada Pav Mirchi",
-    lunch: "White Vatana Aaloo | Dry Gavar | Dal Panchratna | Steam Rice",
-    snacks: "Kachie Dabeli",
-    dinner: "White Vatana Aaloo | Dry Gavar | Dal Panchratna | Steam Rice",
-    midnight: "Veg Puff"
-};
+const MENU_URL =
+"https://ramesh0708.github.io/ideas-food-menu-api/";
 
-document.getElementById("date").innerText =
-    "📅 " + data.date;
+fetch(MENU_URL)
+  .then(response => response.json())
+  .then(data => {
 
-document.getElementById("breakfast").innerText =
-    data.breakfast;
+    document.getElementById("date").innerText =
+      "📅 " + data.date;
 
-document.getElementById("lunch").innerText =
-    data.lunch;
+    document.getElementById("breakfast").innerText =
+      data.breakfast;
 
-document.getElementById("snacks").innerText =
-    data.snacks;
+    document.getElementById("lunch").innerText =
+      data.lunch;
 
-document.getElementById("dinner").innerText =
-    data.dinner;
+    document.getElementById("snacks").innerText =
+      data.snacks;
 
-document.getElementById("midnight").innerText =
-    data.midnight;
+    document.getElementById("dinner").innerText =
+      data.dinner;
+
+    document.getElementById("midnight").innerText =
+      data.midnight;
+
+  })
+  .catch(error => {
+      console.error(error);
+  });
